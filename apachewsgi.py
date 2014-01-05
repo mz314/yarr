208cfef97d6a815bdb0fce94b30fefe0a3cd9697
unknown <maciej@z.xx>
2014-01-05 19:30:11 +0100

    x

--- apachewsgi.py
+++ apachewsgi.py
@@ -6,6 +6,6 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'yarr.settings'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
 
-path = '/media/sf_m/programming/yarr/yarr'
+path = '/var/www/localhost/htdocs/priv/yarr'
 if path not in sys.path:
     sys.path.append(path)
